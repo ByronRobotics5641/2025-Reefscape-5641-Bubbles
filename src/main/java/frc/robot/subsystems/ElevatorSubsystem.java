f2c20a7f88c79eaffb395f929dac7f8c1f6d924e
ByronRobotics5641 <166260278+ByronRobotics5641@users.noreply.github.com>
2025-03-19 17:38:27 -0400

    PID change ele

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -189,7 +189,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
     else if(count == 4) {
 
-      this.setpoint = -109;
+      this.setpoint = -103;
       //System.out.println("Elevator Coral Intake");
     }
     else if(count == 5) {
